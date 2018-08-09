b6ae2e67737cabf70ce45fb75ded137495bf0d4a
Oranye Nonso <nonicalx@gmail.com>
2018-08-09 21:10:45 +0100

    initial

---
+++ LandingPage.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="assets\css\bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="assets/css/fontawesome-all.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/flaticon.css">
+
+		<!-- carousel css include -->
+		<link rel="stylesheet" type="text/css" href="assets/css/slick.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/slick-theme.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/owl.carousel.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/owl.theme.default.min.css">
+
+		<!-- others css include -->
+		<link rel="stylesheet" type="text/css" href="assets/css/magnific-popup.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/jquery.mCustomScrollbar.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/calendar.css">
+
+		<!-- color switcher css include -->
+		<link id="color_theme" rel="stylesheet" type="text/css" href="assets/css/colors/default.css">
+
+		<!-- custom css include -->
+		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+		
+</head>
+<body>
+<div class="row">
+<div class="col-md4" id="logo">
+        <span><img src="IMG-1044.JPG" width="500" height="650"></span>
+</div>  
+<div class="col-md8">
+    <div class="countdown-timer mb-30">
+    <ul class="countdown-list" data-countdown="2018/01/01">
+    <li class="timer-item days">
+    <strong>00</strong><small>days</small></li><li class="timer-item hours">
+    <strong>00</strong><small>hours</small></li><li class="timer-item mins">
+    <strong>00</strong><small>mins</small></li><li class="timer-item seco">
+    <strong>00</strong><small>seco</small></li>
+    </ul>
+	    </div>
+        <center>
+        <div class="title">
+        <h1>WELOCOME<h1>
+        <h1>TO</h1>
+        <h1>THE GOLIBE FETIVAL</h1>
+        </div>
+        <input type="button" value="Process to Site" class="btn btn-info">
+        </center>
+        </div>
+</div>
+ 
+</body>
+</html>
