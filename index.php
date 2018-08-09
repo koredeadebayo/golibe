bd944ff1027587778c1daf8aa6f2155c67bcd9e1
unknown <korvictech@gmail.com>
2018-08-09 20:18:24 +0100

    Updates on Homepage

--- index.php
+++ index.php
@@ -21,205 +21,9 @@
 		================================================== -->
 		<header id="header-section" class="header-section default-header-section auto-hide-header clearfix">
 
-			<!-- header-top - start -->
-			<div class="header-top">
-				<div class="container">
-					<div class="row">
-
-						<!-- basic-contact - start -->
-						<div class="col-lg-6">
-							<div class="basic-contact">
-								<ul>
-									<li>
-										<a href="#!">
-											<i class="fas fa-envelope"></i>
-											info@golibefestival.com
-										</a>
-									</li>
-
-									<li>
-										<a href="#!">
-											<i class="fas fa-phone"></i>
-											+234 813 378 8796
-										</a>
-									</li>
-									
-								</ul>
-							</div>
-						</div>
-						<!-- basic-contact - end -->
-
-						<!-- register-login-group - start -->
-						<div class="col-lg-6">
-							<div class="register-login-group">
-								<ul>
-									<li>
-										<a href="#register-modal" class="register-modal-btn">
-											<i class="fas fa-user"></i>
-											Register
-										</a>
-										<div id="register-modal" class="reglog-modal-wrapper register-modal mfp-hide clearfix" style="background-image: url(assets/images/login-modal-bg.jpg);">
-											<div class="overlay-black clearfix">
-
-												<!-- leftside-content - start -->
-												<div class="leftside-content">
-													<div class="site-logo-wrapper mb-80">
-														<a href="#!" class="">
-															<img src="assets/images/2.site-logo.png" alt="logo_not_found">
-														</a>
-													</div>
-													<div class="register-login-link mb-80">
-														<ul>
-															<li><a href="#!">Login</a></li>
-															<li class="active"><a href="#!">Register</a></li>
-														</ul>
-													</div>
-													<div class="copyright-text">
-														<p class="m-0">©2018 <a href="#!" class="yellow-color">golibefestival.com</a> all right reserved, made with <i class="fas fa-heart"></i> by jThemes Studio </p>
-													</div>
-												</div>
-												<!-- leftside-content - end -->
-
-												<!-- rightside-content - start -->
-												<div class="rightside-content text-center">
-
-													<div class="mb-30">
-														<h2 class="form-title title-large white-color">Account <strong>Register</strong></h2>
-														<span class="form-subtitle white-color">Have an account? <strong>LOGIN NOW</strong></span>
-													</div>
-
-													<div class="login-form text-center mb-50">
-														<form action="#!">
-															<div class="form-item">
-																<input type="email" placeholder="User Name">
-															</div>
-															<div class="form-item">
-																<input type="password" placeholder="Password">
-															</div>
-															<div class="form-item">
-																<input type="email" placeholder="Repeat Password">
-															</div>
-															<div class="form-item">
-																<input type="password" placeholder="Email Address">
-															<div class="human-verification text-left">
-																<input type="checkbox" id="imnotarobot">
-															</div>
-																<label for="imnotarobot">I'm not a robot</label>
-																<span class="verification-image">
-																	<img src="assets/images/iamnotrobot.png" alt="Image_not_found">
-																</span>
-															</div>
-															<button type="submit" class="login-btn">login now</button>
-														</form>
-													</div>
-
-													<div class="bottom-text white-color">
-														<p class="m-0">
-															* Denotes mandatory field.
-														</p>
-														<p class="m-0">** At least one telephone number is required.</p>
-													</div>
-
-												</div>
-												<!-- rightside-content - end -->
-
-												<a class="popup-modal-dismiss" href="#!">
-													<i class="fas fa-times"></i>
-												</a>
-
-											</div>
-										</div>
-									</li>
-									<li>
-										<a href="#login-modal" class="login-modal-btn">
-											<i class="fas fa-lock"></i>
-											Login
-										</a>
-										<div id="login-modal" class="reglog-modal-wrapper mfp-hide clearfix" style="background-image: url(assets/images/login-modal-bg.jpg);">
-											<div class="overlay-black clearfix">
-
-												<!-- leftside-content - start -->
-												<div class="leftside-content">
-													<div class="site-logo-wrapper mb-80">
-														<a href="#!" class="logo">
-															<img src="assets/images/2.site-logo.png" alt="logo_not_found">
-														</a>
-													</div>
-													<div class="register-login-link mb-80">
-														<ul>
-															<li class="active"><a href="#!">Login</a></li>
-															<li><a href="#!">Register</a></li>
-														</ul>
-													</div>
-													<div class="copyright-text">
-														<p class="m-0">©2018 <a href="#!" class="yellow-color">golibefestival.com</a> all right reserved, made with <i class="fas fa-heart"></i> Golibe Festival </p>
-													</div>
-												</div>
-												<!-- leftside-content - end -->
-
-												<!-- rightside-content - start -->
-												<div class="rightside-content text-center">
-
-													<div class="mb-30">
-														<h2 class="form-title title-large white-color">Account <strong>Login</strong></h2>
-														<span class="form-subtitle white-color">Login to our website, or <strong>REGISTER</strong></span>
-													</div>
-
-													<div class="fb-login-btn mb-30">
-														<a href="#!">
-															<span class="icon">
-																<i class="fab fa-facebook-f"></i>
-															</span>
-															login with facebook
-														</a>
-													</div>
-
-													<div class="or-text mb-30">
-														<span>or sign in</span>
-													</div>
-
-													<div class="login-form text-center mb-50">
-														<form action="#!">
-															<div class="form-item">
-																<input type="email" placeholder="example@gmail.com">
-															</div>
-															<div class="form-item">
-																<input type="password" placeholder="Password">
-															</div>
-															<button type="submit" class="login-btn">login now</button>
-														</form>
-													</div>
-
-													<div class="bottom-text white-color">
-														<p class="m-0">
-															* Denotes mandatory field.
-														</p>
-														<p class="m-0">** At least one telephone number is required.</p>
-													</div>
-
-												</div>
-												<!-- rightside-content - end -->
-
-												<a class="popup-modal-dismiss" href="#!">
-													<i class="fas fa-times"></i>
-												</a>
-
-											</div>
-										</div>
-									</li>
-								</ul>
-							</div>
-						</div>
-						<!-- register-login-group - end -->
-						
-					</div>
-				</div>
-			</div>
-			<!-- header-top - end -->
-
 			<!-- header-bottom - start -->
 			<div class="header-bottom">
-				<div class="container">
+				<div class="container ">
 					<div class="row">
 
 						<!-- site-logo-wrapper - start -->
@@ -250,12 +54,17 @@
 												<li class="menu-item-has-children">
 													<a href="#!">events</a>
 												</li>
-												<li class="menu-item-has-children">
-													<a href="#!">FAQs</a>
-												</li>
+												
 												<li>
 													<a href="#">gallery</a>
 												</li>
+												<li class="menu-item-has-children">
+													<a href="#!">FAQs</a>
+												</li>
+												<li class="menu-item-has-children">
+													<a href="#!">Our Team</a>
+												</li>
+												
 												
 												<li class="menu-item-has-children">
 													<a href="#!">contact</a>
@@ -356,6 +165,13 @@
 									Events
 								</a>
 							</li>
+							
+							<li>
+								<a href="#">
+									<span class="icon"><i class="fas fa-home"></i></span>
+									Gallery
+								</a>
+							</li>
 							<li>
 								<a href="#">
 									<span class="icon"><i class="fas fa-home"></i></span>
@@ -365,7 +181,7 @@
 							<li>
 								<a href="#">
 									<span class="icon"><i class="fas fa-home"></i></span>
-									Gallery
+									Our Team
 								</a>
 							</li>
 							<li>
@@ -603,12 +419,9 @@
 							<div class="col-lg-8">
 								<div class="slider-content">
 									<div class="countdown-timer mb-30">
-										<ul class="countdown-list" data-countdown="2018/01/01"></ul>
+										<ul class="countdown-list" data-countdown="2018/12/24"></ul>
 									</div>
 									<span class="date">24 december 2018</span>
-									<h1 class="title-text">
-										ONITSHA
-									</h1>
 									<strong class="bold-text">GOLIBE FESTIVAL</strong>
 									<a href="#!" class="custom-btn">booking ticket</a>
 									<a href="#!" class="details-btn">event details</a>
@@ -847,7 +660,7 @@
 		</section>
 		<!-- event-gallery-section - end
 		================================================== -->
-		<?php include("include/header.php"); ?>
+		<?php include("include/footer.php"); ?>
 
 
 
