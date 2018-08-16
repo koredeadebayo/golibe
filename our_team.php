785d9afe0495464c98e7d763830dad27ea93e74f
nonicalx <nonicalx@gmail.com>
2018-08-16 14:59:07 +0100

    update

--- our_team.php
+++ our_team.php
@@ -15,51 +15,75 @@
 				<div class="background-event">  
 					<h2 class="text-center">Our Team</h2>  
 				</div>
-				<h3 class="text-center">Meet the Team</h3>
+				<h3 class="text-center" style="font-family: tahoma">Meet the Team</h3>
 				<div class="row">
 					
 					<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-						<picture class="curve">
+					<div class="thumb-image">
 							<img src="assets/images/gallery/a9.jpg" alt="team member" class="curve">
-						</picture>
+							<div class="img-caption curve">
+                            	<p>Mr Golibe</p>
+							</div>
+						</div>
 					</div>
 					<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-						<picture>
-							<img src="assets/images/gallery/a9.jpg" alt="team member" class ="curve">
-						</picture>
+					<div class="thumb-image">
+							<img src="assets/images/gallery/a9.jpg" alt="team member" class="curve">
+							<div class="img-caption curve">
+                            	<p>Mr Golibe</p>
+							</div>
+						</div>
 					</div>
 					<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-						<picture>
-								<img src="assets/images/gallery/a9.jpg" alt="team member" class ="curve">
-						</picture>
+					<div class="thumb-image">
+							<img src="assets/images/gallery/a9.jpg" alt="team member" class="curve">
+							<div class="img-caption curve">
+                            	<p>Mr Golibe</p>
+							</div>
+						</div>
 					</div>
 					<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-						<picture>
-							<img src="assets/images/gallery/a9.jpg" alt="team member" class ="curve">
-						</picture>
-					</div>	
+						<div class="thumb-image">
+							<img src="assets/images/gallery/a9.jpg" alt="team member" class="curve">
+							<div class="img-caption curve">
+                            	<p>Mr Golibe</p>
+							</div>
+						</div>
+					</div>
 				</div>
 				<div class="row">
 					
 					<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-						<picture >
-							<img src="assets/images/gallery/a9.jpg" alt="team member" class ="curve">
-						</picture>
+						<div class="thumb-image">
+							<img src="assets/images/gallery/a9.jpg" alt="team member" class="curve">
+							<div class="img-caption curve">
+                            	<p>Mr Golibe</p>
+							</div>
+						</div>
 					</div>
 					<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-						<picture>
-							<img src="assets/images/gallery/a9.jpg" alt="team member" class ="curve">
-						</picture>
+						<div class="thumb-image">
+							<img src="assets/images/gallery/a9.jpg" alt="team member" class="curve">
+							<div class="img-caption curve">
+                            	<p>Mr Golibe</p>
+							</div>
+						</div>
 					</div>
 					<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-						<picture>
-							<img src="assets/images/gallery/a9.jpg" alt="team member" class ="curve">
-						</picture>
+						<div class="thumb-image">
+							<img src="assets/images/gallery/a9.jpg" alt="team member" class="curve">
+							<div class="img-caption curve">
+                            	<p>Mr Golibe</p>
+							</div>
+						</div>
 					</div>
 					<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-						<picture >
-							<img src="assets/images/gallery/a9.jpg" alt="team member" class ="curve">
-						</picture>
+						<div class="thumb-image">
+							<img src="assets/images/gallery/a9.jpg" alt="team member" class="curve">
+							<div class="img-caption curve">
+                            	<p>Mr Golibe</p>
+							</div>
+                    	</div>
 					</div>
 			</div>
 		</section>
@@ -72,63 +96,18 @@
 				<div class="row">
                     <div class="col-md-6 col-sm-12">
                         <p class="about-event">
-                        The village football tournament has always been an annual event.
-                        It will commence on 1st December 2018 and run till the finale on 26th December 2018.
-                        The 2018 edition will include post football event open musical performance at the
-                        arena and with at food &  drinks at the food courts also in the arena
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus malesuada mauris nisi, eget condimentum dolor vehicula id. In hac habitasse platea dictumst. In molestie, ligula id vestibulum congue, leo lorem pulvinar justo, ac porttitor ligula elit ut ligula. Donec sed sem tempor, feugiat nulla quis, tincidunt lacus. Aliquam tempor euismod lacus, at dapibus mauris commodo sed. Integer pulvinar ac mi a placerat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed orci nisi, aliquam quis lectus vitae, mollis pharetra odio.
                         </p>
-                        <ul class="list-group about-info">
-                            <li class="list-group-item">Begins on : 1, December, 2018 </li>
-                            <li class="list-group-item">Closes on: 26, December, 2018 </li>
-                            <li class="list-group-item">Venue: The Stadium</li>
-                        </ul>
                     </div>
 
                     <div class="col-md-6 col-sm-12">
                         <div class="text-center">
-                         <h2 class="text-center hidden-xs">Footballer's picture</h2>
+                         <h2 class="text-center hidden-xs">Sponsors</h2>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
-
-        <section id="aboutcontent2" class="aboutcontent2 sec-ptb-100 clearfix">
-            <div class="container">
-            <h1 class="text-center">Prices</h1>
-                <div class="row">
-                    <div class="col-sm-4 col-md-4">
-                        <div class="thumbnail text-center">
-                            <i class="fas fa-trophy price1"></i>
-                            <div class="caption">
-                                <h3>First Place</h3>
-                                <p>...</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-md-4">
-                        <div class="thumbnail text-center">
-                            <i class="fas fa-trophy price2"></i>
-                            <div class="caption">
-                                <h3>Second Place</h3>
-                                <p>...</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-md-4">
-                        <div class="thumbnail text-center">
-                            <i class="fas fa-trophy price3"></i>
-                            <div class="caption">
-                                <h3>Third Place</h3>
-                                <p>...</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-		</section>
-		<!-- About content - end
-        ================================================== -->
 
 
 
